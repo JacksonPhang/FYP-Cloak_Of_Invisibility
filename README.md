ea66c6d31f4774c5fd71237642d01feac458e2a8
Jacknitor <jackson.phang99@yahoo.com>
2020-12-15 09:45:41 +0800

    Added script to setup libraries. TBC to subprocess

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FYP-Cloak_Of_Invisibility
+
+## Setting up libraries for PyTorch implementation
+
+```shell
+python Setup_Libraries.py
+```
---
+++ Setup_Libraries.py
@@ -0,0 +1,10 @@
+import os
+
+def install():
+    #install required libraries
+    os.system('python -m pip install --upgrade pip')
+    os.system('pip install torch')
+    os.system('pip install torchvision')
+
+if __name__ == "__main__":
+    install()
