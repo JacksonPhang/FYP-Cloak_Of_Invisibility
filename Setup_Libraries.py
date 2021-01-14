488faf3ef828f7027fc56bfcacfc2b61d4ec2063
Jacknitor <jackson.phang99@yahoo.com>
2021-01-14 17:26:58 +0800

    added installation of matplotlib to setup script

--- Setup_Libraries.py
+++ Setup_Libraries.py
@@ -2,6 +2,7 @@ import os
 
 def install():
     #install required libraries
+    os.system('pip install --upgrade matplotlib')
     os.system('python -m pip install --upgrade pip')
     os.system('pip install torch')
     os.system('pip install torchvision')
