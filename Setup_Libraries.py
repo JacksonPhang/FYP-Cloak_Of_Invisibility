ef1c571a22679ba8067f1fba38eaa291ffee8917
Jacknitor <jackson.phang99@yahoo.com>
2021-01-14 23:33:25 +0800

    added installation for pyqt5

--- Setup_Libraries.py
+++ Setup_Libraries.py
@@ -4,6 +4,7 @@ def install():
     #install required libraries
     os.system('python -m pip install --upgrade pip')
     os.system('pip install --upgrade matplotlib')
+    os.system('pip install --upgrade PyQt5')
     os.system('pip install torch')
     os.system('pip install torchvision')
 
